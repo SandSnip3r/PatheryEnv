4dc09c6d9dd065d5d1daec379eb6014c44e871d7
SandSnip3r <sandsnip3r@gmail.com>
2024-08-20 16:30:06 -0700

    Finish most of pathery

--- pathery_env/envs/pathery.py
+++ pathery_env/envs/pathery.py
@@ -19,106 +19,69 @@ class CellType(Enum):
   GOAL = 4
 
 class PatheryEnv(gym.Env):
-  metadata = {"render_modes": ["human", "rgb_array", "ansi"], "render_fps": 4}
+  metadata = {"render_modes": ["ansi"], "render_fps": 4}
 
   def __init__(self, render_mode=None):
-    self.size = 4  # The size of the square grid
-    self.grid_size = (self.size, self.size)
-    self.grid = np.zeros(self.grid_size, dtype=np.int32)  # Initialize grid with OPEN cells
+    # Initialize grid size
+    self.grid_size = (2, 6)
+
+    # Initialize grid with OPEN cells
+    self.grid = np.zeros(self.grid_size, dtype=np.int32)
 
     # Observation space: Each cell type is a discrete value
     self.observation_space = spaces.MultiDiscrete(np.full((self.grid_size[0], self.grid_size[1]), len(CellType)))
 
     self.window_size = 512  # The size of the PyGame window
 
-    # We have 4 actions, corresponding to "right", "up", "left", "down", "right"
-    self.action_space = spaces.Discrete(4)
-
-    """
-    The following dictionary maps abstract actions from `self.action_space` to 
-    the direction we will walk in if that action is taken.
-    i.e. 0 corresponds to "right", 1 to "up" etc.
-    """
-    self._action_to_direction = {
-      Actions.right.value: np.array([1, 0]),
-      Actions.up.value: np.array([0, 1]),
-      Actions.left.value: np.array([-1, 0]),
-      Actions.down.value: np.array([0, -1]),
-    }
+    # Possible actions are which 2d position to place a block in
+    self.action_space = spaces.MultiDiscrete((self.grid_size[0], self.grid_size[1]))
 
     assert render_mode is None or render_mode in self.metadata["render_modes"]
     self.render_mode = render_mode
-
-    """
-    If human-rendering is used, `self.window` will be a reference
-    to the window that we draw to. `self.clock` will be a clock that is used
-    to ensure that the environment is rendered at the correct framerate in
-    human-mode. They will remain `None` until human-mode is used for the
-    first time.
-    """
-    self.window = None
-    self.clock = None
 
   def _get_obs(self):
     return self.grid
 
   def _get_info(self):
-    return {
-      "distance": np.linalg.norm(
-        self._agent_location - self._target_location, ord=1
-      )
-    }
+    return {}
 
   def reset(self, seed=None, options=None):
     # We need the following line to seed self.np_random
     super().reset(seed=seed)
 
-    # Start in top left
+    # Set the number of blocks that the user can place
+    self.remainingBlocks = 3
+
+    # Place the start in top left
     self.grid[0][0] = CellType.START.value
-    # End in bottom right
+
+    # Place the end in bottom right
     self.grid[self.grid_size[0]-1][self.grid_size[1]-1] = CellType.GOAL.value
-
-    # Choose the agent's location uniformly at random
-    self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
-
-    # We will sample the target's location randomly until it does not
-    # coincide with the agent's location
-    self._target_location = self._agent_location
-    while np.array_equal(self._target_location, self._agent_location):
-      self._target_location = self.np_random.integers(
-        0, self.size, size=2, dtype=int
-      )
 
     observation = self._get_obs()
     info = self._get_info()
-
-    if self.render_mode == "human":
-      self._render_frame()
 
     return observation, info
 
+  def calculateShortestPath(self):
+    return 0
+
   def step(self, action):
-    # Map the action (element of {0,1,2,3}) to the direction we walk in
-    direction = self._action_to_direction[action]
-    # We use `np.clip` to make sure we don't leave the grid
-    self._agent_location = np.clip(
-      self._agent_location + direction, 0, self.size - 1
-    )
-    # An episode is done iff the agent has reached the target
-    terminated = np.array_equal(self._agent_location, self._target_location)
-    reward = 1 if terminated else 0  # Binary sparse rewards
+    if self.grid[action[0]][action[1]] == CellType.OPEN.value:
+      self.grid[action[0]][action[1]] = CellType.BLOCKED_PLAYER_PLACED.value
+      self.remainingBlocks -= 1
+    else:
+      return self._get_obs(), -1, False, False, self._get_info()
+    
+    terminated = self.remainingBlocks == 0
+    reward = self.calculateShortestPath() if terminated else 0
     observation = self._get_obs()
     info = self._get_info()
-
-    if self.render_mode == "human":
-      self._render_frame()
 
     return observation, reward, terminated, False, info
 
   def render(self):
-    if self.render_mode == "rgb_array":
-      return self._render_frame()
-    elif self.render_mode == "ansi":
+    if self.render_mode == "ansi":
       return self._render_ansi()
 
   def _render_ansi(self):
@@ -129,75 +92,13 @@ class PatheryEnv(gym.Env):
       CellType.START: 'S',  # Start
       CellType.GOAL: 'G'   # Goal
     }
-    top_border = "+" + "-" * (self.size * 2 - 1) + "+"
+    top_border = "+" + "-" * (self.grid_size[1] * 2 - 1) + "+"
     output = top_border + '\n'
     for row in self.grid:
       output += '|' + '|'.join(ansi_map[CellType(val)] for val in row) + '|\n'
-    output += top_border
+    output += top_border + '\n'
+    output += f'Remaining blocks: {self.remainingBlocks}'
     return output
-    
-  def _render_frame(self):
-    if self.window is None and self.render_mode == "human":
-      pygame.init()
-      pygame.display.init()
-      self.window = pygame.display.set_mode((self.window_size, self.window_size))
-    if self.clock is None and self.render_mode == "human":
-      self.clock = pygame.time.Clock()
-
-    canvas = pygame.Surface((self.window_size, self.window_size))
-    canvas.fill((255, 255, 255))
-    pix_square_size = (
-      self.window_size / self.size
-    )  # The size of a single grid square in pixels
-
-    # First we draw the target
-    pygame.draw.rect(
-      canvas,
-      (255, 0, 0),
-      pygame.Rect(
-        pix_square_size * self._target_location,
-        (pix_square_size, pix_square_size),
-      ),
-    )
-    # Now we draw the agent
-    pygame.draw.circle(
-      canvas,
-      (0, 0, 255),
-      (self._agent_location + 0.5) * pix_square_size,
-      pix_square_size / 3,
-    )
-
-    # Finally, add some gridlines
-    for x in range(self.size + 1):
-      pygame.draw.line(
-        canvas,
-        0,
-        (0, pix_square_size * x),
-        (self.window_size, pix_square_size * x),
-        width=3,
-      )
-      pygame.draw.line(
-        canvas,
-        0,
-        (pix_square_size * x, 0),
-        (pix_square_size * x, self.window_size),
-        width=3,
-      )
-
-    if self.render_mode == "human":
-      # The following line copies our drawings from `canvas` to the visible window
-      self.window.blit(canvas, canvas.get_rect())
-      pygame.event.pump()
-      pygame.display.update()
-
-      # We need to ensure that human-rendering occurs at the predefined framerate.
-      # The following line will automatically add a delay to
-      # keep the framerate stable.
-      self.clock.tick(self.metadata["render_fps"])
-    else:  # rgb_array
-      return np.transpose(
-        np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
-      )
 
   def close(self):
     if self.window is not None:
--- run_env.py
+++ run_env.py
@@ -1,15 +1,24 @@
-import gymnasium
+import gymnasium as gym
 import pathery_env
 from enum import Enum
 import numpy as np
-env = gymnasium.make('pathery_env/Pathery-v0', render_mode='ansi')
+env = gym.make('pathery_env/Pathery-v0', render_mode='ansi')
 
 env.reset()
 print(env.render())
 
+terminated = False
+while not terminated:
+  reward = -1
+  while reward < 0:
+    action = env.action_space.sample()
+    _, reward, terminated, _, _ = env.step(action)
+  print(env.render())
+  # return observation, reward, terminated, False, info
+
 # low = np.array([0, 0, 0], dtype=int)
 # high = np.array([1, 10, 100], dtype=int)
-# space = gymnasium.spaces.Box(low=low, high=high, dtype=int)
+# space = gym.spaces.Box(low=low, high=high, dtype=int)
 # for _ in range(100):
 #   print(space.sample())
 
@@ -23,7 +32,11 @@ print(env.render())
 
 # width = 4
 # height = 5
-# testSpace = gymnasium.spaces.MultiDiscrete(np.full((width, height), len(EnumType)))
-# # testSpace = gymnasium.spaces.MultiDiscrete(([len(EnumType)]*width)*height)
+# testSpace = gym.spaces.MultiDiscrete(np.full((width, height), len(EnumType)))
+# # testSpace = gym.spaces.MultiDiscrete(([len(EnumType)]*width)*height)
 # print(testSpace)
 # print(testSpace.sample())
+
+# x = gym.spaces.MultiDiscrete((10,2))
+# print(x.sample())
+# print(x.sample())
